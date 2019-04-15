b4cabc8efba47e24412caff09a46312b574f41fe
jasoncarcamo <47464765+jasoncarcamo@users.noreply.github.com>
2019-04-15 12:19:28 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+
+function displayChart(response){
+    $('#wordlist').empty();
+    if(response.length){
+        $('#container').css('display','block')
+        for(let i = 0; i < response.length; i++){
+            $('#wordlist').append(`<li>${response[i].word}</li>`)
+        }
+
+    }else{
+        $('#container').css('display', 'none');
+    }
+}
+
+function getWords(){
+
+    let ourinput= $('#userword').val();
+    let ouroption = $('#relatedto').val()
+    let url = '';
+    if(ouroption){
+        url = `http://api.datamuse.com/words?rel_rhy=${ourinput}&ml=${ouroption}`;
+    }else{
+        url = `http://api.datamuse.com/words?rel_rhy=${ourinput}`;
+    }
+    
+    
+    fetch(url)
+    .then(response => response.json())
+    .then(responseJson => {
+        if(responseJson.length === 0){
+            throw new Error('No words found, did you type in a real word?')
+        }
+    
+        displayChart(responseJson);
+
+        console.log(responseJson);
+    })
+    .catch((error)=> alert(error));
+}
+
+
+$('form').submit(function(event){
+    event.preventDefault();
+    getWords();
+})
